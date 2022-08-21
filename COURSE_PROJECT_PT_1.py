0cd0f37de9baf8f3089fdaac9b21487d59a256be
alxnjalex <alxnjalex@aol.com>
2022-08-21 04:34:59 -0700

    Part 3 complete

--- COURSE_PROJECT_PT_1.py
+++ COURSE_PROJECT_PT_1.py
@@ -1,22 +1,3 @@
-total = {
-            'employee' : 0,
-            'hours' : 0,
-            'tax' : 0,
-            'net' : 0
-          }
-
-all_employee_name = []
-all_total_hours = []
-all_hourly_rate = []
-all_income_tax_rate = []
-all_income_tax = []
-all_net_pay = []
-all_to_date = []
-all_from_date = []
-all_gross_pay = []
-
-
-
 def get_employee_name():
     employee_name = input("Input employee name: ")
     return employee_name
@@ -39,23 +20,6 @@ def get_net_pay(total_hours, hourly_rate, income_tax_rate):
     net_pay = gross_pay - income_tax
     return income_tax, net_pay, gross_pay
 
-def display_employee(employee_name, total_hours, hourly_rate, income_tax_rate, income_tax, net_pay, gross_pay, from_day, to_day):
-    print("Employee Name : ", employee_name)
-    print("Total Hours : ", total_hours)
-    print("Hourly Rate : ", hourly_rate)
-    print("Income Tax Rate : ", income_tax_rate)
-    print("Income tax : ", income_tax)
-    print("Income net : ", net_pay)
-    print("Gross pay : ", gross_pay)
-    print("From day : ", from_day)
-    print("To day : ", to_day)
-
-def display_all_employee():
-    print(f"Total Employee : {total_employee}")
-    print(f"Total Hours : {total_employee_hours}")
-    print(f"Total Tax : {total_employee_tax}")
-    print(f"Total Net Pay : {total_employee_net_pay}")
-
 def get_date():
     from_date = ''
     to_date = ''
@@ -65,42 +29,94 @@ def get_date():
     to_date = input("input to date: ")
     return from_date, to_date
 
+def get_from_date(filename):
+    total = {
+        'employee' : 0,
+        'hours' : 0,
+        'tax' : 0,
+        'net_pay' : 0,
+    }
+    with open(filename, "r") as f:
+        from_date = input("From Date : ")
+
+        if (from_date == "All"):
+            print("here")
+            for line in f:
+                words = line.split('|')
+                print("Employee Name : ", words[2])
+                print("Total Hours : ", words[3])
+                print("Hourly Rate : ", words[4])
+                print("Income Tax Rate : ", words[5])
+                print("Income tax : ", words[6])
+                print("Income net : ", words[7])
+                print("Gross pay : ", words[8])
+                print("From day : ", words[0])          
+                print("To day : ", words[1])
+                
+                total['employee'] += 1
+                total['hours'] += int(float(words[3]))
+                total['tax'] += int(float(words[6]))
+                total['net_pay'] += int(float(words[7]))
+        else:
+            for line in f:
+                words = line.split('|')
+                if words[0] == from_date:
+                    print("Employee Name : ", words[2])
+                    print("Total Hours : ", words[3])
+                    print("Hourly Rate : ", words[4])
+                    print("Income Tax Rate : ", words[5])
+                    print("Income tax : ", words[6])
+                    print("Income net : ", words[7])
+                    print("Gross pay : ", words[8])
+                    print("From day : ", words[0])
+                    print("To day : ", words[1])
+                    
+                    total['employee'] += 1                   
+                    total['hours'] += int(float(words[3]))   
+                    total['tax'] += int(float(words[6]))     
+                    total['net_pay'] += int(float(words[7])) 
+
+        print("Total Employee = ", total["employee"])
+        print("Total Hours = ", total["hours"])
+        print("Total Tax = ", total["tax"])
+        print("Total Net Pay = ", total["net_pay"])
 
 
 
-while True:
-    from_date, to_date = get_date()
-    if(from_date == "End."):
-        break
-    all_from_date.append(from_date)
-    all_to_date.append(to_date)
-    employee_name = get_employee_name()
-    all_employee_name.append(employee_name)
-    total_hours = get_total_hours()
-    all_total_hours.append(total_hours)
-    hourly_rate = get_hourly_rate()
-    all_hourly_rate.append(hourly_rate)
-    income_tax_rate = get_income_tax_rate()
-    all_income_tax_rate.append(income_tax_rate)
-    income_tax, net_pay, gross_pay = get_net_pay(total_hours, hourly_rate, income_tax_rate)
-    all_income_tax.append(income_tax)
-    all_net_pay.append(net_pay)
-    all_gross_pay.append(gross_pay)
 
-# when the loop is terminated
+filename = "employee.txt"
 
-for i in range(len(all_employee_name)):
-    display_employee(all_employee_name[i], all_total_hours[i], all_hourly_rate[i], all_income_tax_rate[i], all_income_tax[i], all_net_pay[i], all_gross_pay[i], all_from_date[i], all_to_date[i])
-    total['employee'] += 1
-    total['hours'] += all_total_hours[i]
-    total['tax'] += all_income_tax[i]
-    total['net'] += all_net_pay[i]
+with open(filename, "a") as f:
+    while True:
+        from_date, to_date = get_date()
+        if (from_date == "End."):
+            break
+        f.write(from_date)  #0
+        f.write('|')
+        f.write(to_date)    #1
+        f.write('|')
+        employee_name = get_employee_name()
+        f.write(employee_name)    #2
+        f.write('|')
+        total_hours = get_total_hours()
+        f.write(str(total_hours))   #3
+        f.write('|')
+        hourly_rate = get_hourly_rate()
+        f.write(str(hourly_rate))    #4
+        f.write('|')
+        income_tax_rate = get_income_tax_rate()
+        f.write(str(income_tax_rate))    #5
+        f.write('|')
+        income_tax, net_pay, gross_pay = get_net_pay(total_hours, hourly_rate, income_tax_rate)
+        f.write(str(income_tax))  #6
+        f.write('|')
+        f.write(str(net_pay))     #7
+        f.write('|')
+        f.write(str(gross_pay))    #8
+        f.write('\n')
+        
+get_from_date(filename)
 
-
-print("Total Employee : {}".format(total['employee']))
-print("Total Hours : {}".format(total['hours']))
-print("Total Income Tax : {}".format(total['tax']))
-print("Total Net Pay : {}".format(total['net']))
 
 
 
